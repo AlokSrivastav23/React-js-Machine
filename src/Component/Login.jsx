fa07168057376427be3f318688657a7a98646d62
Alok Srivastav <97796215+AlokSrivastav23@users.noreply.github.com>
2022-07-29 23:12:12 +0530

    Add files via upload

---
+++ src/Component/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+// import './style.css';
+function Login() {
+  const [Username, setUserName] = useState('');
+  const [Password, SetPassword] = useState('');
+ const SubmitHandler = (e) => {
+    e.preventDefault();
+    alert('Login Page');
+  };
+  return (
+    <>
+      <form onSubmit={SubmitHandler} >
+          <br/><br/><br/>
+      <h1>This Is Login Page </h1>
+        <input
+          type="text" id="email" maxlength="50" placeholder="E-mail" required
+          name="email"
+          value={Username}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="text" id="password" placeholder="Password" required
+          name="password"
+          value={Password}
+          onChange={(e) => SetPassword(e.target.value)}
+        />
+        <button className='loginBtn'>Login </button>
+        <span><Link to="Welcome">
+         <h1 className='Loginnextpage'>Welcome</h1> 
+        </Link></span>
+      </form>
+    </>
+  );
+}
+export default Login;
